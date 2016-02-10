29c7b5fbfd6034f04e98ca958eacb88bb77db1e5
Your Name <you@example.com>
2016-02-10 23:03:01 +0200

    tryit2

---
+++ pyhtonEducation/graph/pageRankMapper.py
@@ -0,0 +1,11 @@
+import sys
+import re
+
+for row in sys.stdin:
+    print(row.strip())
+    key, totalWeigth, outs = row.strip().split('\t')
+    outVertex = [int(i) for i in re.findall(r"[\w]+", outs)]
+    outNumber = len(outVertex)
+    outWeigth = format(float(totalWeigth)/outNumber, '.3f')
+    for v in outVertex:
+        print(str(v) +'\t' + str(outWeigth) + '\t{}')
