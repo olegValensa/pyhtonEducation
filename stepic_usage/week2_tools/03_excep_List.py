96b67e1abcb77661c83961a5aacf9f3aa99446a2
Oleg Maksymenko <maol@ciklum.com>
2016-04-23 16:58:29 +0300

    week2 updates

--- stepic_usage/week2_tools/03_excep_List.py
+++ stepic_usage/week2_tools/03_excep_List.py
@@ -1,2 +1,17 @@
-class PositiveList(list):
+class NonPositiveError(Exception):
     pass
+
+class PositiveList(list):
+    def append(self, x):
+        if x > 0:
+            super(PositiveList, self).append(x)
+        else:
+            raise NonPositiveError(str(x) + " is negative")
+
+a = PositiveList([1, 2])
+print(a)
+a.append(4)
+print(a)
+a.append(0)
+print(a)
+a.append(-1)
